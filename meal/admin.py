70a0079e0a0be68340ddd59a31f84d4017fc939c
Lizzie Davison <lizzie@hubblehq.com>
2022-03-21 00:42:48 +0000

    Basic meal plan structure
    I would prefer to get food items from the bring api but it seems
    unlikely, the only thing exposed is lists and list management. Which
    don't get me wrong makes total sense, the way they manage food is pretty
    much their entire app model ahha.

--- meal/admin.py
+++ meal/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meal, FoodItem, ShoppingListItem
+
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+  list_display = ("name", "created_at", "updated_at")
+  search_fields = ("name",)
+
+class ShoppingListItemInline(admin.TabularInline):
+    model = ShoppingListItem
+    extra = 0
+    min_num = 1
+    autocomplete_fields = ['food_item']
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+  list_display = ("id", "date", "name", "created_at", "updated_at")
+  search_fields = ("date", "name")
+  inlines = [
+    ShoppingListItemInline
+  ]
--- meal/models.py
+++ meal/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+  date = models.DateField()
+  name = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class FoodItem(models.Model):
+  name = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
+
+class ShoppingListItem(models.Model):
+  food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+  meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+  note = models.CharField(max_length=255, blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+
